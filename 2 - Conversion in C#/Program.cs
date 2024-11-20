3e980723059a68f1152ec0bd8e5e113a188fc2d6
shark0o <shahrokhrahmati8@gmail.com>
2024-11-20 13:22:29 +0200

    Add files via upload

---
+++ 2 - Conversion in C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___Conversion_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberAsText = "1a2c3";
+            int numberAsIntger = 231;
+            int.Parse(numberAsText);
+
+            Console.WriteLine(numberAsText);
+            Console.WriteLine(numberAsIntger);
+
+
+            
+        }
+    }
+}
