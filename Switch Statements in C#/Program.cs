3b8e5249a6573ee9337fa2ad68302617d40f1ce4
shark0o <shahrokhrahmati8@gmail.com>
2024-11-25 13:46:14 +0200

    Add files via upload

---
+++ Switch Statements in C#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statements_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your grade");
+            int grade = Convert.ToInt32(Console.ReadLine());
+            switch (grade)
+            {
+                case 0:
+                    Console.WriteLine("Fail")
+                        ; break;
+                case 1:
+                    Console.WriteLine("Needs Improvement");
+                    break;
+                case 2:
+                    Console.WriteLine("Ok");
+                    break;
+                case 3:
+                    Console.WriteLine("Satisfactory");
+                    break;
+                case 4:
+                    Console.WriteLine("Good");
+                    break;
+                case 5:
+                    Console.WriteLine("Excellent");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Switch Statements in C#/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Switch Statements in C#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Switch Statements in C#")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c0e7b30-aa57-409d-bab2-5d78176bdc3b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
