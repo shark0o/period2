d18b7d34d2729734542971c7bdb6cf43360f4fda
shark0o <shahrokhrahmati8@gmail.com>
2024-11-26 12:00:35 +0200

    Add files via upload

---
+++ while loops in C#/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loops_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Counter = 1;
+            while (Counter <= 5)
+            {
+                Console.WriteLine($"Counter value: {Counter}");
+                Counter++;
+            }
+            Console.WriteLine("The loop has finished.");
+            Console.ReadLine();
+        }
+    }
+}
