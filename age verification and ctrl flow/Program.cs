454c1ed4a442c70b120f07e881f1aa0462580737
shark0o <shahrokhrahmati8@gmail.com>
2024-12-10 11:43:11 +0200

    Add files via upload

---
+++ age verification and ctrl flow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace age_verification_and_ctrl_flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+
+            if (age >= 17; i )
+            {
+                Console.WriteLine("your not old enough");
+                Console.ReadLine ();
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
